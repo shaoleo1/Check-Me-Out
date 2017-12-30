0e5e7640762d3f39776b30bd2b7ced6f37cf1a96
Leo Shao <shaoleo1@gmail.com>
2017-12-30 18:26:18 -0500

    Implemented checked out books and added map

--- LibraryApp/LibraryApp/FirstViewController.swift
+++ LibraryApp/LibraryApp/FirstViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
+    
+    var refreshControl: UIRefreshControl!
+    
+    var myBooks: [String] = []
+    
+    @IBOutlet weak var bookTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -36,9 +45,12 @@ class FirstViewController: UIViewController {
                             for book in data {
                                 if let title = book["title"] as? String {
                                     if let author = book["author"] as? String {
-                                        
+                                        self.myBooks.append("\(title) - \(author)")
                                     }
                                 }
+                            }
+                            DispatchQueue.main.async{
+                                self.bookTableView.reloadData()
                             }
                         }
                     }
@@ -48,13 +60,92 @@ class FirstViewController: UIViewController {
             }
         })
         task.resume()
+        self.bookTableView.register(UITableViewCell.self, forCellReuseIdentifier: self.cellReuseIdentifier)
+        
+        // (optional) include this line if you want to remove the extra empty cell divider lines
+        // self.tableView.tableFooterView = UIView()
+        
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        self.bookTableView.delegate = self
+        self.bookTableView.dataSource = self
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        bookTableView.refreshControl = refreshControl
+        bookTableView.addSubview(refreshControl)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet weak var SearchMyBooks: UISearchBar!
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.myBooks.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = self.bookTableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.myBooks[indexPath.row]
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
+    @objc func refresh(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        let sid = defaults.object(forKey: "sid") as? String
+        var request = URLRequest(url: URL(string: "http://52.22.1.14:3000/library/api/v1/checkedout?sid=" + sid! + "&key=bsvr9N5wrGJVDz98UvBMnGt8")!)
+        // Sets the http method to GET which means GETting data FROM the API. There are two methods, GET and POST. POST means POSTing data TO the API. In this case, we're using GET.
+        request.httpMethod = "GET"
+        // Sets the file type that the data will be retrieved to be JSON, which is the standard format.
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Starts the HTTP session (connects to the API URL with the search query and GETs the data).
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            do {
+                // Converts and saves the returned data into a variable called 'json' in appropriate JSON formatting.
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                // Creates a dictionary from the JSON file to look up the value for the key given.
+                if let array = json as? [Any] {
+                    // Creates an array of the returned terms.
+                    if let dict = array[0] as? [String: Any] {
+                        // Creates a dictionary of the current term details (e.g. term, definition, term number).
+                        if let data = dict["data"] as? [[String: Any]] {
+                            self.myBooks = []
+                            for book in data {
+                                if let title = book["title"] as? String {
+                                    if let author = book["author"] as? String {
+                                        self.myBooks.append("\(title) - \(author)")
+                                    }
+                                }
+                            }
+                            DispatchQueue.main.async {
+                                self.bookTableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print("error")
+            }
+        })
+        task.resume()
+        DispatchQueue.main.async {
+            self.refreshControl.endRefreshing()
+        }
+    }
 }
 
