440800c935923eac87e7cc2c6bd1a3e2aafe823e
Leo Shao <shaoleo1@gmail.com>
2018-01-07 16:42:57 -0500

    Wiped repo

--- LibraryApp/LibraryApp/DiscoveredBarCodeView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DiscoveredBarCodeView.swift
-//  LibraryApp
-//
-//  Created by Vicky Zheng on 12/26/17.
-//  Copyright © 2017 Vicky Zheng. All rights reserved.
-//
-
-import Foundation
--- LibraryApp/LibraryApp/LoginVC.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LoginVC.swift
-//  LibraryApp
-//
-//  Created by Vicky Zheng on 12/27/17.
-//  Copyright © 2017 Vicky Zheng. All rights reserved.
-//
-
-import Foundation
